70d03532975252bd9982beba60a8720e11ec8f02
bstee615 <benjaminjsteenhoek@gmail.com>
2020-04-17 05:05:16 +0000

    Version 2

--- Prog.c
+++ Prog.c
@@ -2,10 +2,11 @@
 int main() {
   float a, b, area;
   scanf("%f %f", &a, &b);
-  area = .5 * a * b;
-  if (area > 0)
+  area = (a * b) / 2;
+  if (area > 0) {
     printf("The area of (%.4f,%.4f),(%.4f,0) and (0,%.4f) is %.4f.", a, b, a, b, area);
-  else
-    printf("The area of (%.4f,%.4f),(%.4f,0) and (0,%.4f) is %.4f.", a, b, a, b, -area);
+  } else {
+    printf("The area of (%.4f,%.4f),(%.4f,0) and (0,%.4f) is %.4f.", a, b, a, b, (-1) * area);
+  }
   return 0;
 }
